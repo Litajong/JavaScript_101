4c1a7878ee6b3218bdf99d2f019479fa6c815cbd
litajong <Litajong@gmail.com>
2020-05-01 17:07:31 -0500

    conditional statement

---
+++ 03_conditional_statement.js
@@ -0,0 +1,21 @@
+// If/ Else Statements
+var x = 25;
+if (x > 50) {
+    console.log("bigger than 50");
+}
+else {
+    console.log("small than 50");
+}
+
+
+// If/ Else IF / Else Statements
+var x = 25;
+if (x > 100) {
+    console.log("bigger than 100");
+}
+else if (x > 50) {
+    console.log("bigger than 50");
+}
+else {
+    console.log("small number");
+}
