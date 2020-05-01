bead00ba1cab29cdd1f4d39c524ff4008dc2a855
litajong <Litajong@gmail.com>
2020-05-01 14:23:01 -0500

    no message

---
+++ 01_variables_and_datatype.js
@@ -0,0 +1,31 @@
+// print out
+console.log("hello!");
+
+// Data types
+// strings
+var myStringVariable = "Hello there!";
+console.log(myStringVariable);
+
+// numbers
+var myNum = 5;
+var myOtherNum = 10;
+console.log (myNum + myOtherNum);
+
+// undefined
+var myVar;
+console.log(myVar);
+
+// boolean
+var myBool = true;
+console.log(myBool);
+
+//null
+var z = null;
+console.log(z);
+
+// other example
+var x = 5;
+var y = 6;
+x = y;
+console.log(x)
+console.log(y)
