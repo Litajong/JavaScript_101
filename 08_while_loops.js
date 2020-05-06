6254adebcf14524ba0da04c39775affc7b440b03
litajong <Litajong@gmail.com>
2020-05-06 16:13:44 -0500

    while loops

---
+++ 08_while_loops.js
@@ -0,0 +1,18 @@
+// a for loop is usually used when repeating a process a known number of times
+for (var i = 0; i < 10; i ++){
+    console.log(i);
+}
+
+// a while loop is usually used when repeating a process until some condition is met
+var i = 0
+while (i < 10){
+    console.log(i);
+    i ++;
+}
+
+// example
+var num = 0
+while (num <=5){
+    console.log(num);
+    num ++;
+}
